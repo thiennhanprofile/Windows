1e932be403ce9e98d15e11746a28f4da96166649
thiennhanprofile <nguyenthiennhan188@gmail.com>
2023-09-30 20:28:37 +0700

    changes

---
+++ Buoi2_1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buoi02_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+
+            Console.Write("Nhap tong so sinh vien :");
+            int n = Convert.ToInt32(Console.ReadLine());
+            SinhVien[] arrSinhVien = new SinhVien[n];
+            Console.WriteLine("\n =====> Nhap danh sach sinh vien <=======");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Nhap sinh vien thu {0}", i + 1);
+                arrSinhVien[i] = new SinhVien();
+                arrSinhVien[i].NhapSV();
+            }
+            Console.WriteLine("\n =====> Xuat danh sach <=====");
+            foreach (SinhVien sv in arrSinhVien)
+            {
+                sv.XuatSV();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Buoi2_1/SinhVien.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buoi02_1
+{
+    internal class SinhVien
+    {
+        private string _MaSV;
+        public string MaSV
+        {
+            get { return _MaSV; }
+            set { _MaSV = value; }
+
+        }
+        private string _HoTen;
+        public string HoTen
+        {
+            get { return _HoTen; }
+            private set { _HoTen = value; }
+        }
+        private double _DiemTB;
+        public double DiemTB
+        {
+            get { return _DiemTB; }
+            private set { _DiemTB = value; }
+        }
+        private string _Khoa;
+        public string Khoa
+        {
+            get { return _Khoa; }
+            private set { _Khoa = value; }
+        }
+        public SinhVien() { }
+        public SinhVien(string msv, string ht, double diem, string khoa)
+        {
+            this.MaSV = msv;
+            this.HoTen = ht;
+            this.DiemTB = diem;
+            this.Khoa = khoa;
+        }
+        public SinhVien(SinhVien sinhVien)
+        {
+            this.MaSV = sinhVien.MaSV;
+            this.HoTen = sinhVien.HoTen;
+            this.DiemTB = sinhVien.DiemTB;
+            this.Khoa = sinhVien.Khoa;
+        }
+        public void NhapSV()
+        {
+            Console.Write("nhap ma sinh vien");
+            MaSV = Console.ReadLine();
+            Console.Write("nhap ho ten sinh vien");
+            HoTen = Console.ReadLine();
+            Console.Write("nhap diem trung binh sinh vien");
+            DiemTB = double.Parse(Console.ReadLine());
+            Console.Write("nhap khoa sinh vien");
+            Khoa = Console.ReadLine();
+
+        }
+        public void XuatSV()
+        {
+            Console.WriteLine("{0} \t{1} \t{2} \t{3} \n", MaSV, HoTen, DiemTB, Khoa);
+        }
+    }
+}
